64fc808ff34258890e1e49d2d513527602674d65
filippomeluzzi09 <filippomeluzzi09@gmail.com>
2026-05-26 18:22:21 +0200

    setup iniziale

--- cupid_adventures.py
+++ cupid_adventures.py
@@ -1,3 +1,17 @@
 #Manila Baldini 
 #Maria Fontana
 #Filippo Meluzzi
+
+# Cupid Adventures 3D - Full Leaderboard & Menu Button Edition ❤️
+# pip install pygame moderngl numpy
+
+import pygame
+import random
+import math
+import moderngl
+import array
+import numpy as np
+import os
+
+pygame.init()
+pygame.mixer.init()
